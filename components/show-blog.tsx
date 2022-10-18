4e8b262950e0660c2dcd354f03fc00e90ba1eb33
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-10-18 22:23:20 +0300

    style

--- components/show-blog.tsx
+++ components/show-blog.tsx
@@ -5,19 +5,19 @@ import CommunityAction from "./community-action";
 export default function ShowBlog({blog}:{blog:Blog}) {
     return (
         <>
-            <Card border="danger" >
+            <Card border="secondary" >
                 <Card.Header>
                     <Stack direction="horizontal" >
-                        <Card.Title className="mb-2 mt-2 text-muted"><h3>{blog.header}</h3></Card.Title>
-                        <Stack direction="vertical" className="ms-auto">
-                            <p className="text-muted">Yazar</p>
-                            <p className="text-secondary ms-auto">{blog.writer}</p>
+                        <Card.Title className="mb-2 mt-2"><h3>{blog.header}</h3></Card.Title>
+                        <Stack direction="vertical" className="ms-auto p-0 m-0">
+                            <p className="text-muted ms-auto p-0 m-0">Yazar</p>
+                            <p className="text-secondary ms-auto p-0 m-0">{blog.writer}</p>
                         </Stack>
                         
                     </Stack>
                 </Card.Header>
                 <Card.Body>
-                <p className="mb-2 text-muted">{blog.content}</p>
+                <p className="mb-2 mt-3 text-light">{blog.content}</p>
                 </Card.Body>
                 <Card.Footer className="pb-3 ">
                    <CommunityAction blog={blog} />
