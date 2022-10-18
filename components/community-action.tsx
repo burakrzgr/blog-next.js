b18cbd6cccc61416894ac6a17a2c6375b6ca3fca
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-10-18 22:22:55 +0300

    style

--- components/community-action.tsx
+++ components/community-action.tsx
@@ -4,7 +4,7 @@ import { Blog } from "../types/blog";
 export default function CommunityAction({blog}:{blog:Blog}) {
     return (
         <Stack direction="horizontal" >
-            
+            <div></div>
         </Stack>
     );
 }
