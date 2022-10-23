cd330f32d2805616ca2ce5937fe3000a6b3469db
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-10-23 11:36:51 +0300

    neyse

--- components/community-action.tsx
+++ components/community-action.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Stack } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 import { CommunityInfo } from "../types/blog";
 import CommentDisplay from "./comment-display";
 
@@ -7,11 +7,11 @@ export default function CommunityAction({info}:{info:CommunityInfo}) {
     return (
         <div>
             <Stack direction="horizontal" >
-                <div className="me-2 btn btn-lg btn-primary"><Image className="" src="/svg/like.svg" alt="Like Icon" width={20} height={20} /><span className="m-0 ms-1">{info.likes} Beğeni</span></div> 
-                <div className="me-2 btn btn-lg btn-primary"><Image src="/svg/heart.svg" alt="Love Icon" width={20} height={20} /><span className="m-0 ms-1">{info.loves} Harika</span></div> 
-                <div className="me-2 btn btn-lg btn-secondary"><Image src="/svg/dislike.svg" alt="Dislike Icon" width={20} height={20} /><span className="m-0 ms-1">{info.dislikes} Beğenmeme</span></div> 
-                <div className="me-2 btn btn-lg btn-danger ms-5"><Image src="/svg/speech.svg" alt="Dislike Icon" width={20} height={20} /><span className="m-0 ms-1">Yorum Yaz</span></div> 
-                <div className="me-2 ms-auto btn btn-lg btn-secondary"><Image src="/svg/more.svg" alt="More Icon" width={20} height={20} /></div>
+                <Button variant="primary" size="lg" className="me-2"><Image className="" src="/svg/like.svg" alt="Like Icon" width={20} height={20} /><span className="m-0 ms-1">{info.likes} Beğeni</span></Button> 
+                <Button variant="primary" size="lg" className="me-2"><Image src="/svg/heart.svg" alt="Love Icon" width={20} height={20} /><span className="m-0 ms-1">{info.loves} Hayran KAlma</span></Button> 
+                <Button variant="secondary" size="lg" className="me-2"><Image src="/svg/dislike.svg" alt="Dislike Icon" width={20} height={20} /><span className="m-0 ms-1">{info.dislikes} Yuhlama</span></Button> 
+                <Button variant="danger" size="lg" className="me-2 ms-5" ><Image src="/svg/speech.svg" alt="Comment Icon" width={20} height={20} /><span className="m-0 ms-1">Yorum Yaz</span></Button> 
+                <Button variant="secondary" size="lg" className="me-2 ms-auto" ><Image src="/svg/more.svg" alt="More Icon" width={20} height={20} /></Button>
             </Stack>
             <div className="me-2 mt-4 text-muted">
                 <span className="m-0 ms-1">
