1f7a417dcb15d5c5e4d36af0c32821ea6b1bf4a1
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-10-27 00:09:05 +0300

    text

--- components/community-action.tsx
+++ components/community-action.tsx
@@ -29,7 +29,7 @@ export default function CommunityAction({blogId,info}:{blogId:string,info:Commun
         <div>
             <Stack direction="horizontal" >
                 <Button variant="primary" size="lg" className="me-2"><Image className="" src="/svg/like.svg" alt="Like Icon" width={20} height={20} /><span className="m-0 ms-1">{info.likes} Beğeni</span></Button> 
-                <Button variant="primary" size="lg" className="me-2"><Image src="/svg/heart.svg" alt="Love Icon" width={20} height={20} /><span className="m-0 ms-1">{info.loves} Hayransı</span></Button> 
+                <Button variant="primary" size="lg" className="me-2"><Image src="/svg/heart.svg" alt="Love Icon" width={20} height={20} /><span className="m-0 ms-1">{info.loves} Hayran</span></Button> 
                 <Button variant="secondary" size="lg" className="me-2"><Image src="/svg/dislike.svg" alt="Dislike Icon" width={20} height={20} /><span className="m-0 ms-1">{info.dislikes} Yuhlama</span></Button> 
                 <Button variant="danger" size="lg" className="me-2 ms-5" onClick={() =>{setCommenting(!commenting)}}><Image src="/svg/speech.svg" alt="Comment Icon" width={20} height={20} /><span className="m-0 ms-1">{commenting?"Neyse Vazgeçtim":"Yorum Yaz"}</span></Button> 
                 <Button variant="secondary" size="lg" className="me-2 ms-auto" ><Image src="/svg/more.svg" alt="More Icon" width={20} height={20} /></Button>
