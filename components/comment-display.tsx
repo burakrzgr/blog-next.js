b74ca9760fa18877c279109943f3173ea1814f38
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-10-29 23:53:31 +0300

    removed

--- components/comment-display.tsx
+++ components/comment-display.tsx
@@ -46,27 +46,7 @@ export default function CommentDisplay({ blogId }: { comments: any,blogId:string
             {(comments.length ?? 0) === 0 ?
                 <div>Henüz kimse yorum yazmadı. Düşüncelerini paylaşan ilk kişi sen ol.</div> :
                 comments.length - count > 0 ?
-                    <div role="button" className="text-decoration-underline" onClick={() => setCount(count + 2)}>{comments.length - count} yorumu {count > 0 ? "daha" : ""} görüntüleyin.</div> : <></>}
+                    <div role="button" className="text-decoration-underline" onClick={() => setCount(count + 10)}>{comments.length - count} yorumu {count > 0 ? "daha" : ""} görüntüleyin.</div> : <></>}
         </>
-
-
-       /*<>
-
-
-            {Object.keys(comments).map((id, inx) => { 
-                if (inx >= count) return; return (
-                <Stack key={inx} className="border rounded-1 p-2 m-1" direction="horizontal">
-                    <div >
-                        <div className="badge bg-danger">{comments[id].writer}</div>
-                        <div className="mt-1">{comments[id].content}</div>
-                    </div>
-                    <div className="ms-auto mb-auto badge bg-info" >{comments[id].date?comments[id].date.toDate().toLocaleString('tr-TR'):"Tarih Yok"}</div>
-                </Stack>
-            ) })}
-            {(numOfcom?? 0) === 0 ?
-                <div>Henüz kimse yorum yazmadı. Düşüncelerini paylaşan ilk kişi sen ol.</div> :
-                numOfcom - count > 0 ?
-                    <div role="button" className="text-decoration-underline" onClick={() => setCount(count + 15)}>{numOfcom - count} yorumu {count > 0 ? "daha" : ""} görüntüleyin.</div> : <></>}
-        </>*/
     );
 }
