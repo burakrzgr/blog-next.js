589037820e380e036fe20cf3247b1a5bc16bd5c5
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-10-30 15:25:36 +0300

    login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
+import { FirebaseError } from 'firebase/app';
 import Router from 'next/router';
 import { useState } from 'react';
-import { Button, Card, Container, Form, Stack } from 'react-bootstrap';
+import { Alert, Button, Card, Container, Form, Stack } from 'react-bootstrap';
 import { useAuth } from '../context/auth-context';
 import styles from '../styles/Home.module.css'
 
@@ -12,15 +13,40 @@ export default function Login() {
     const [password, setPassword] = useState("");
     const [passwordAgain, setPasswordAgain] = useState("");
     const [username, setUsername] = useState("");
+    const [error,setError] = useState("");
 
     async function submitHandler() {
+        if(!isLogginIn && password !== passwordAgain){
+            setError("Şifreler aynı değil!");
+            return;
+        }
+        let redirect = false;
         if (isLogginIn) {
-            await login(email, password);
+            try{
+                await login(email, password);
+                redirect = true;
+            }
+            catch(err){
+                if(err instanceof FirebaseError){
+                    setError(err.code);
+                }
+            }
+                //.then(() => Router.push("/read")).error((ex:any) => console.log("1",ex));
         }
         else {
-            await signup(email, password);
+            try{
+                await signup(email, password);
+                redirect = true;
+            }
+            catch(err){
+                if(err instanceof FirebaseError){
+                    setError(err.code);
+                }
+            }
         }
-        await Router.push("/read");
+        if(redirect){
+            await Router.push("/read");
+        }
     }
 
     return (
@@ -28,32 +54,37 @@ export default function Login() {
             <Container style={{ width: "35rem" }}>
                 <Card>
                     <Card.Body>
-                        <div className='text-auto'>
-                            <div className='' style={{ width: "10rem" }}>{isLogginIn ? <h3>Login</h3> : <h3>Register</h3>}</div>
+                        <div className='text-center'>
+                            {isLogginIn ? <h2>Giriş</h2> : <h2>Kayıt</h2>}
                         </div>
-                        <Stack direction='vertical'>
+                        <Stack direction='vertical' gap={2} className="p-2">
+                            {error.length>0?<Alert variant='danger'>{error}</Alert>:<></>}
                             {!isLogginIn ?
-                                <>
-                                    <Form.Label>Username</Form.Label>
+                                <Form.Group>
+                                    <Form.Label>Kullanıcı Adı</Form.Label>
                                     <Form.Control onChange={(e) => setUsername(e.target.value)} value={username}></Form.Control>
-                                </> : <></>
+                                </Form.Group> : <></>
                             }
-                            <Form.Label>E-Mail</Form.Label>
-                            <Form.Control onChange={(e) => setEmail(e.target.value)} value={email}></Form.Control>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' onChange={(e) => setPassword(e.target.value)} value={password}></Form.Control>
+                            <Form.Group>
+                                <Form.Label>E-Mail</Form.Label>
+                                <Form.Control onChange={(e) => setEmail(e.target.value)} value={email}></Form.Control>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Şifre</Form.Label>
+                                <Form.Control type='password' onChange={(e) => setPassword(e.target.value)} value={password}></Form.Control>
+                            </Form.Group>
                             {!isLogginIn ?
-                                <>
-                                    <Form.Label>Password Again</Form.Label>
+                                <Form.Group>
+                                    <Form.Label>Şifre Tekrarı</Form.Label>
                                     <Form.Control type='password' onChange={(e) => setPasswordAgain(e.target.value)} value={passwordAgain}></Form.Control>
-                                </> : <></>
+                                </Form.Group> : <></>
                             }
                         </Stack>
                     </Card.Body>
                     <Card.Footer>
-                        <Stack direction='horizontal'>
+                        <Stack direction='horizontal' className='p-2'>
                             <Button variant='outline-secondary' onClick={() => setIsLogginIn(!isLogginIn)}>Hesabın Yok mu?</Button>
-                            <Button variant='primary ms-auto' onClick={() => submitHandler()}> Giriş Yap</Button>
+                            <Button variant='primary ms-auto' onClick={() => submitHandler()}> {isLogginIn ?<>Giriş Yap</>:<>Kayıt Ol</>}</Button>
                         </Stack>
                     </Card.Footer>
                 </Card>
