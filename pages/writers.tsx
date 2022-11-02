c2fb9f77174641a6cf9c3a09baeaaf816dc13be1
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-11-02 22:18:36 +0300

    writer

--- pages/writers.tsx
+++ pages/writers.tsx
@@ -1,6 +1,8 @@
 import { collection, getDocs } from 'firebase/firestore';
-import * as React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Router } from 'next/router';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { InterestDisplay } from '../components/interest-display';
 import { database } from '../config/firebase-config';
 import styles from '../styles/Home.module.css'
@@ -9,8 +11,8 @@ import styles from '../styles/Home.module.css'
 const dbInstance = collection(database, 'writers');
 
 export default function Writers ({}) {
-    const [writers, setWriters] = React.useState<{load:boolean,users:any[]}>({load:false,users:[]});
-    React.useEffect(() => {
+    const [writers, setWriters] = useState<{load:boolean,users:any[]}>({load:false,users:[]});
+    useEffect(() => {
         getData();
     }, []);
 
@@ -28,9 +30,10 @@ export default function Writers ({}) {
             <h3>List of Writers</h3>
             {writers.users.map((x,k) => {return (<div key={k}>
                     <Row className='border myborder m-2 p-2'>
-                        <Col sm={12} lg={4}>{x.writerId}</Col>
-                        <Col sm={12} lg={4}>{x.nickname}</Col>
+                        <Col sm={6} lg={3}>{x.writerId}</Col>
+                        <Col sm={6} lg={3}>{x.nickname}</Col>
                         <Col sm={12} lg={4}><InterestDisplay interests={x.interests}></InterestDisplay></Col>
+                        <Col sm={12} lg={2}><Button onClick={() => Router.push("/writer/"+x.writerId)}></Button></Col>
                     </Row>
             </div>)})}
         </Container>
