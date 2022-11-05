34d435021ac2ad870ef6aec5e1cb3f733e65fb4d
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-11-05 22:26:52 +0300

    writer page

--- components/writer-blog.tsx
+++ components/writer-blog.tsx
@@ -1,25 +1,23 @@
-import { query, where } from 'firebase/firestore';
-import * as React from 'react';
+import { collection, documentId, getDocs, query, where } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+import { database } from '../config/firebase-config';
 import { Blog, BlogWriter } from '../types/blog';
 import ShowBlog from './show-blog';
 
-
+const dbInstance = collection(database, 'blogs');
 
 export default function WriterBlog ({writer}: {writer:BlogWriter}) {
     const [blogs, setBlogs] = useState<{load:boolean,message:string,blog:Blog[]}>({load:false,message:'',blog:[]});
-
     useEffect(() => {
-        let q2 = query(dbInstance,where(documentId(),'in',writer.blogs))
-        getDocs(q2).then((dat2) => {
-            setBlogs({load:true,message:'',blog:dat2.docs.map(x => { 
-                return {blogId:x.id,...x.data()} as Blog })});
-        });
+        if(writer.blogs.length>0){
+            let q2 = query(dbInstance,where(documentId(),'in',writer.blogs))
+            getDocs(q2).then((dat2) => {
+                setBlogs({load:true,message:'',blog:dat2.docs.map(x => { 
+                    return {blogId:x.id,...x.data()} as Blog })});
+            });
+        }
+    }, [writer.blogs])
     
-    }, [third])
-    
-
-
     return (
         <>
             {blogs.load?
--- pages/writer/[writerId].tsx
+++ pages/writer/[writerId].tsx
@@ -2,18 +2,16 @@ import { collection, doc, documentId, getDoc, getDocs, limit, query, where } fro
 import { useRouter } from "next/router";
 import { useEffect,useState } from "react";
 import { Badge, Col, Container, Row } from "react-bootstrap";
-import ShowBlog from "../../components/show-blog";
 import WriterBlog from "../../components/writer-blog";
 import { database } from "../../config/firebase-config";
 import { useAuth } from "../../context/auth-context";
 import styles from '../../styles/Home.module.css'
-import { Blog, BlogWriter } from "../../types/blog";
+import { BlogWriter } from "../../types/blog";
 
-const dbInstance = collection(database, 'blogs');
+
 
 export default function WriterInfo({ }) {
     const [writer, setWriter] = useState<BlogWriter>({userId:'',color:'5600F2',image:'Non',username:'Loading',blogs:[]});
-    const [blogs, setBlogs] = useState<{load:boolean,message:string,blog:Blog[]}>({load:false,message:'',blog:[]});
     const router = useRouter();
     const {user} = useAuth();
     const { writerId } = router.query;
@@ -34,18 +32,16 @@ export default function WriterInfo({ }) {
       }
 
 
-
-
     return (
         <main className={styles.main} >
             <Container className={styles.container}>
                 <Row>
-                    <Col col={3}>
+                    <Col xs={12} sm={3}>
                         <h3>{writer.username}</h3>
                         {writer.userId === user.uid?<Badge bg='info'>Its you :D</Badge>:<Badge bg='danger'>Report</Badge>}
                         <p>{writerId}</p>
                     </Col>
-                    <Col col={9}>
+                    <Col xs={12} sm={9}>
                         <WriterBlog writer={writer}></WriterBlog>
                     </Col>
                 </Row>
