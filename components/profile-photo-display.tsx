6d4c7f61d697766bd0b2fdc70774c3b04b9d4ac2
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-11-06 00:29:02 +0300

    images

--- components/profile-photo-display.tsx
+++ components/profile-photo-display.tsx
@@ -8,7 +8,7 @@ export default function ProfilePhotoDisplay({ link }: { link?: string }) {
     return (
         <div className='d-flex justify-content-center'>
             {link?
-                <div className={styles.useImg + ' w-75'} style={{ aspectRatio: '1/1', backgroundImage:link }}></div>:
+                <div className={styles.useImg + ' w-75'} style={{ aspectRatio: '1/1', backgroundImage:`/img/pp/${link}`}}></div>:
                 <div className={styles.useImg + ' w-75'} style={{ aspectRatio: '1/1', ...noPhoto }}></div>
             }
         </div>
