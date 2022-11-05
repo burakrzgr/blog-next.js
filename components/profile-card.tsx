1c1fdfaf486706b66b817a68a060b3bb4f0be5bf
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-11-06 00:56:35 +0300

    profile

--- components/profile-card.tsx
+++ components/profile-card.tsx
@@ -1,4 +1,4 @@
-import { Badge, Stack } from "react-bootstrap";
+import { Badge, Button, Stack } from "react-bootstrap";
 import { useAuth } from "../context/auth-context";
 import { BlogWriter } from "../types/blog";
 import { InterestDisplay } from "./interest-display";
@@ -14,12 +14,18 @@ export default function ProfileCard ({writer}:{writer:BlogWriter}) {
         </div>
         <div>
             <div className="text-end">
-                {writer.userId === user.uid?<Badge bg='info'>Its you :D</Badge>:<Badge bg='danger'>Report</Badge>}
+                {writer.userId === user.uid?<Badge bg='info'>Its you :D</Badge>:<Badge bg='danger' role='button'>Report</Badge>}
             </div>
             <h2>{writer.username}</h2>
         </div>
         <div>
             <InterestDisplay interests={writer.interests}></InterestDisplay>
+        </div>
+        <div>
+            <Stack direction="horizontal">
+                <div className="link-info" role="button">0 kişi takipçisi</div><div className="ms-4 link-info" role="button">0 kişiyi takipte</div>
+                {writer.userId === user.uid?<></>:<Button size="sm" variant="danger" className="ms-auto" role="button">Takibe Al</Button>}
+            </Stack>
         </div>
         <div>
             <h6 className="text-muted">Hakkımda</h6>
