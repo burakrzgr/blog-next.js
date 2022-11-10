1a811b8b0b8d20a0540592af6bda598bb8d7d4f5
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-11-10 23:10:34 +0300

    set

--- components/profile-card.tsx
+++ components/profile-card.tsx
@@ -1,16 +1,26 @@
+import { collection, doc, setDoc, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import { Badge, Button, Form, Stack } from "react-bootstrap";
+import { database } from "../config/firebase-config";
 import { useAuth } from "../context/auth-context";
 import { BlogWriter } from "../types/blog";
 import FollowerAction from "./follower-action";
 import { InterestDisplay } from "./interest-display";
 import ProfilePhotoDisplay from "./profile-photo-display";
 
+const dbInstance = collection(database, 'writers');
 
 export default function ProfileCard ({writer}:{writer:BlogWriter}) {
   const {user} = useAuth();
   const [editMode, setEditMode] = useState(false);
-  
+  const saveProfileInfo = (val : string) => {
+    setDoc(doc(dbInstance,user.writerId),
+        {
+            "desc": "test"
+        },{merge:true}).then(x => 
+    setEditMode(false));
+  }
+
   return (
     <Stack direction="vertical" gap={3}>
         <div>
@@ -20,7 +30,7 @@ export default function ProfileCard ({writer}:{writer:BlogWriter}) {
             <div className="text-end">
                 {writer.userId === user.uid?
                     (editMode?
-                        <Badge bg='success' role='button' onClick={() => setEditMode(false)}>Düzenlemeyi Kaydet</Badge>:
+                        <Badge bg='success' role='button' onClick={() => saveProfileInfo(writer.desc)}>Düzenlemeyi Kaydet</Badge>:
                         <Badge bg='info' role='button' onClick={() => setEditMode(true)}>Profilini düzenle</Badge>):
                     <Badge bg='danger' role='button'>Kullanıcıyı Şikayet Et</Badge>}
             </div>
