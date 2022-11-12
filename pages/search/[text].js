f5a8dabc80d37e2678e7f2732ff1ba50893d48ec
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-11-12 23:44:42 +0300

    arama

--- pages/search/[text].js
+++ pages/search/[text].js
@@ -1,15 +1,23 @@
+import React, { useState,useEffect } from "react";
 import { useRouter } from "next/router";
 import { Container } from "react-bootstrap";
-import styles from '../../styles/Home.module.css'
+import ShowBlogList, {ShowBlogProps} from "../../components/show-blog-list";
+import styles from '../../styles/Home.module.css';
 
 
 export default function SearchPageKeyword({ }) {
-    const router = useRouter()
+    const router = useRouter();
     const { text } = router.query;
+    const [blogs, setBlogs] = useState({load:false,blog:[]});
+    useEffect(() => {
+      setBlogs({load:true,blog:[]})
+    }, [])
+    
     return (
         <main className={styles.main} >
             <Container className={styles.container}>
-                <p>{text}</p>
+                <h5>Arama metini "{text}" için {blogs?.blog?.length??0} sonuç bulundu.</h5>
+                <ShowBlogList blogs={blogs}></ShowBlogList>
             </Container>
         </main>
     );
