58c6a940b41a502da448e5f56321b65e58b4dbe3
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-11-12 22:30:40 +0300

    yazarlar

--- pages/writers.tsx
+++ pages/writers.tsx
@@ -4,8 +4,10 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { InterestDisplay } from '../components/interest-display';
+import ProfilePhotoDisplay from '../components/profile-photo-display';
 import { database } from '../config/firebase-config';
 import styles from '../styles/Home.module.css'
+import { BlogWriter } from '../types/blog';
 
 
 const dbInstance = collection(database, 'writers');
@@ -19,19 +21,26 @@ export default function Writers ({}) {
     const getData = () => {
         getDocs(dbInstance)
             .then((data) => {
-                setWriters({load:true,writers:data.docs.map(x => { return {writerId:x.id,...x.data()} as any })});
+                setWriters({load:true,writers:data.docs.map(x => { return {id:x.id,...x.data()} as BlogWriter })});
             });
     }
   return (
     <main className={styles.main} >
         <Container className={styles.container}>
-            <h3>List of Writers</h3>
+            <h2>Yazarlar</h2>
             {writers.writers.map((x,k) => {return (<div key={k}>
                     <Row className='border myborder m-2 p-2'>
-                        <Col sm={6} lg={3}>{x.writerId}</Col>
-                        <Col sm={6} lg={3}>{x.nickname}</Col>
-                        <Col sm={12} lg={4}><InterestDisplay interests={x.interests}></InterestDisplay></Col>
-                        <Col sm={12} lg={2}><Button variant="dark" onClick={async () => await Router.push(`/writer/${x.writerId}`)}>Görüntüle</Button></Col>
+                        <Col sm={12} lg={3}>
+                            <ProfilePhotoDisplay link={x.image} />
+                        </Col>
+                        <Col sm={12} lg={7}>
+                            <Row><h3>{x.username}</h3></Row>
+                            <Row><InterestDisplay interests={x.interests}></InterestDisplay></Row>
+                            <Row>{x.desc}</Row>
+                        </Col>
+                        <Col sm={12} lg={2}>
+                            <Button variant="dark" className='w-100 h-100 p-3' onClick={async () => await Router.push(`/writer/${x.id}`)}>Görüntüle</Button>
+                        </Col>
                     </Row>
             </div>)})}
         </Container>
