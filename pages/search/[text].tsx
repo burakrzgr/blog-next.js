3014a632fff2a5335851245c087d71280962f501
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-11-12 23:52:14 +0300

    text

--- pages/search/[text].tsx
+++ pages/search/[text].tsx
@@ -3,13 +3,17 @@ import { useRouter } from "next/router";
 import { Container } from "react-bootstrap";
 import ShowBlogList, {ShowBlogProps} from "../../components/show-blog-list";
 import styles from '../../styles/Home.module.css';
+import { collection } from "firebase/firestore";
+import { database } from "../../config/firebase-config";
 
+const dbInstance = collection(database, 'blogs');
 
 export default function SearchPageKeyword({ }) {
     const router = useRouter();
     const { text } = router.query;
-    const [blogs, setBlogs] = useState({load:false,blog:[]});
+    const [blogs, setBlogs] = useState<ShowBlogProps>({load:false,blog:[]});
     useEffect(() => {
+
       setBlogs({load:true,blog:[]})
     }, [])
     
