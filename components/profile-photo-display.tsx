68d2a8acc782c6ad324b942d9f60a7c8c294d253
Burak Rüzgar <burakrzgr@gmail.com>
2022-11-14 15:15:59 +0300

    random

--- components/profile-photo-display.tsx
+++ components/profile-photo-display.tsx
@@ -1,15 +1,19 @@
 import styles from '../styles/Home.module.css'
 
-const noPhoto = {
+const noPhoto = [{
     backgroundImage: "url('/img/noprofile-female.jpg')"
+},
+{
+    backgroundImage: "url('/img/noprofile-male.png')"
 }
+]
 
 export default function ProfilePhotoDisplay({ link }: { link: string }) {
     return (
         <div className='d-flex justify-content-center'>
             {link.length > 0?
                 <div className={styles.useImg + ' w-75'} style={{ aspectRatio: '1/1', backgroundImage:`url('/img/pp/${link}')`}}></div>:
-                <div className={styles.useImg + ' w-75'} style={{ aspectRatio: '1/1', ...noPhoto }}></div>
+                <div className={styles.useImg + ' w-75'} style={{ aspectRatio: '1/1', ...noPhoto[Math.floor(Math.random()*2)] }}></div>
             }
         </div>
     );
